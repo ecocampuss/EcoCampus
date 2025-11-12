a765c268e3f9eb293cbf0e16f94de27539cfa714
ecoCampus <ecocampus.official@gmail.com>
2025-11-12 15:57:44 +0530

    Intial Commit

---
+++ src/main/java/com/ecocampusGroup/ecocampus/EcocampusApplication.java
@@ -0,0 +1,13 @@
+package com.ecocampusGroup.ecocampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecocampusGroup/ecocampus/EcocampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecocampusGroup.ecocampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1531988afa7cacf092feb2b9b2e1f74557885f8a
ecocampus <ecocampus.official@gmail.com>
2025-11-12 20:19:47 +0530

    testing

--- src/main/java/com/ecocampusGroup/ecocampus/EcocampusApplication.java
+++ src/main/java/com/ecocampusGroup/ecocampus/EcocampusApplication.java
@@ -8,6 +8,8 @@ public class EcocampusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcocampusApplication.class, args);
+		System.out.println("hello");
+
 	}
 
 }

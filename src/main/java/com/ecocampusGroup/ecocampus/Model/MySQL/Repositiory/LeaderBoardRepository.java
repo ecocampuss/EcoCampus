5f694f85b8d7aba7110ae35f92fcdb5b35df38de
siddhesh mali <siddheshmali3050@gmail.com>
2025-11-15 11:33:12 +0530

    directory change

--- src/main/java/com/ecocampusGroup/ecocampus/Model/MySQL/Repositiory/LeaderBoardRepository.java
+++ src/main/java/com/ecocampusGroup/ecocampus/Model/MySQL/Repositiory/LeaderBoardRepository.java
@@ -1,7 +1,9 @@
-package com.ecocampusGroup.ecocampus.Model.MySQL.Entities.Repositiory;
+package com.ecocampusGroup.ecocampus.Model.MySQL.Repositiory;
 
 import com.ecocampusGroup.ecocampus.Model.MySQL.Entities.LeaderBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LeaderBoardRepository extends JpaRepository<LeaderBoard, Long> {
 }
